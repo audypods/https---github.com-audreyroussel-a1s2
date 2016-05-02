aa2f0a0da7e17ba46f06887ee1daa8357bb170c8
audypods <avc1.roussel@orange.fr>
2016-05-02 16:19:18 +0200

    ajout des boutons

---
+++ JeuDesSixCouleurs2/src/JeuDesSixCouleurs2/Jeu.java
@@ -0,0 +1,27 @@
+package JeuDesSixCouleurs2;
+import edu.princeton.cs.introcs.StdDraw;
+import java.util.Random;
+
+public class Jeu {
+	
+	/*fonction qui est utilisée pour doner une couleur au hasard à chaque case du plateau
+	 * renvoit hasard (numero attribué à une couleur)
+	 * demande de déssiner la case via StdDraw
+	 */
+	
+		/*public static int Hasard(Lettre a){
+			Random rand = new Random();
+			int hasard = rand.nextInt(6)+0;
+			if (a.getLettre()=='r' || a.getLettre()=='R')
+				StdDraw.setPenColor(StdDraw.RED);
+			else if (a.getLettre()=='b' || a.getLettre()=='B')
+				StdDraw.setPenColor(StdDraw.BLUE);
+			else if (a.getLettre()=='v'|| a.getLettre()=='V')
+				StdDraw.setPenColor(StdDraw.GREEN);
+			else if (a.getLettre()=='j' || a.getLettre()=='J')
+				StdDraw.setPenColor(StdDraw.YELLOW);
+			else if (a.getLettre()=='o' || a.getLettre()=='O')
+				StdDraw.setPenColor(255, 150, 0);
+			return 
+		}*/
+	}
